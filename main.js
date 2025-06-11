c0324a486eae17668f703352a6b9849d2798b263
merxgrc <mgarcia73@mail.sfsu.edu>
2025-06-10 23:10:51 -0700

    added an arrow after each node except the last

--- main.js
+++ main.js
@@ -40,12 +40,26 @@ const list = new LinkedList();
 list.append("Node 1");
 list.append("Node 2");
 list.append("Node 3");
+list.append("Node 4");
 
 // Display the list on the web page
 const container = document.getElementById("listDisplay");
-list.toArray().forEach(val => {
+list.toArray().forEach((value, index, array) => {
   const nodeBox = document.createElement("div");
   nodeBox.className = "node";
-  nodeBox.textContent = val;
+  nodeBox.textContent = value;
   container.appendChild(nodeBox);
+
+    if (index < array.length - 1) {
+        const nodeArrow = document.createElement("div");
+        nodeArrow.className = "arrow";
+
+        const img = document.createElement("img");
+        img.src = "./resources/arrow.png"
+        img.className = "arrowImage"
+
+        nodeArrow.appendChild(img);
+        container.appendChild(nodeArrow);
+    }
+  
 });
